43b73ef85f480a0e66f54b95c1c0f89e51fa0d3c
Mohammed <arrahmaan9901@gmail.com>
2022-11-03 17:15:33 +0530

    wrangler publish, with ioaders not found(png,jpg)

--- src/store/carousal.js
+++ src/store/carousal.js
@@ -2,19 +2,19 @@ export const carousal = {
     "Diaper%20Bag": [
         {
             label: 'Front View',
-            imgPath: require("../img/products/diaperbag/Views/2.jpg"),
+            imgPath: require('../img/products/diaperbag/Views/2.jpg'),
         },
         {
             label: 'Back View',
-            imgPath: require("../img/products/diaperbag/Views/1.jpg"),
+            imgPath: require('../img/products/diaperbag/Views/1.jpg'),
         },
         {
             label: 'Right View',
-            imgPath: require("../img/products/diaperbag/Views/3.jpg"),
+            imgPath: require('../img/products/diaperbag/Views/3.jpg'),
         },
         {
             label: 'Left View',
-            imgPath: require("../img/products/diaperbag/Views/4.jpg"),
+            imgPath: require('../img/products/diaperbag/Views/4.jpg'),
         },
     ],
 
